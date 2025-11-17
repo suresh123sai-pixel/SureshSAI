7fc01cc465a2c7be5888d2dc2be4a8fd739219b2
suresh123sai-pixel <suresh123sai@gmail.com>
2025-11-17 09:57:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
-
-st.title("🎈 ULTIMATE NIFTY PRO 2025")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
